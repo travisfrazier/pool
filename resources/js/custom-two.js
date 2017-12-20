bcee39e2bee9809451e6110d4d8e5383c7c3e727
travisfrazier <travisf777@gmail.com>
2017-12-20 18:21:25 -0500

    updates

--- resources/js/custom-two.js
+++
@@ -1,9 +0,0 @@
-$('#myBtnthree').click(function(){
-    var $this = $(this);
-    $this.toggleClass('SeeMore2');
-    if($this.hasClass('SeeMore2')){
-        $this.text('See More');         
-    } else {
-        $this.text('See Less');
-    }
-});
