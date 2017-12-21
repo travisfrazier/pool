458a2c28896b02aa18498a17da27cd45c18059ac
travisfrazier <travisf777@gmail.com>
2017-12-20 23:18:39 -0500

    updated project with drop nav

--- resources/js/custom-one.js
+++ resources/js/custom-one.js
@@ -1,3 +1,11 @@
+function changeModalText(title, legend){
+    console.log(title, legend)
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('contact').innerHTML = legend;
+}
+
+
+
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -6,6 +14,8 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
+    
+    changeModalText("Aqua Experts", "Fill out the form below and Aqua Experts will get in touch.")
     modal.style.display = "block";
 };
 
@@ -21,6 +31,8 @@ var btn = document.getElementById("myBtntwo");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
+
+    changeModalText("Carolina Pool Pro", "Fill out the form below and Carolina Pool Pro will get in touch.")
     modal.style.display = "block";
 };
 
@@ -44,10 +56,12 @@ span.onclick = function() {
 };
 
 
+// Dropdown Menu 
+$(".mobile-menu").click(function(e) {
+  $(".mobile-nav-list-wrapper").slideToggle();
+});
 
-
-
-
-
-  
+$(".mobile-nav-list-close").click(function(e) {
+    $(".mobile-nav-list-wrapper").slideToggle();
+  });
 
